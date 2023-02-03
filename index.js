2041198b9e99c7e24cf03a1ba94a1d7068e97724
I-Slot <ismailadesholaharun@gmail.com>
2023-02-03 08:52:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ let massEl = document.getElementById("kilogram-convert")
 convertBTN.addEventListener("click", function () {
     console.log("clicked")
     inputEl = inputEl.value
-    lengthEl.textContent = `${inputEl} meters = ${(inputEl * 3.281.toFixed(3))} feets | ${inputEl} feets = ${(inputEl * 0.305.toFixed(3))} meters`;
-    volumeEl.textContent = `${inputEl} liters = ${(inputEl * 0.264.toFixed(3))} gallons | ${inputEl} gallons = ${(inputEl * 3.785.toFixed(3))} liters`;
-    massEl.textContent = `${inputEl} kilos = ${(inputEl * 2.204.toFixed(3))} pounds | ${inputEl} pounds = ${(inputEl * 0.454.toFixed(3))} kilos`;
+    lengthEl.textContent = `${inputEl} meters = ${(inputEl * 3.281).toFixed(3)} feets | ${inputEl} feets = ${(inputEl * 0.305).toFixed(3)} meters`;
+    volumeEl.textContent = `${inputEl} liters = ${(inputEl * 0.264).toFixed(3)} gallons | ${inputEl} gallons = ${(inputEl * 3.785).toFixed(3)} liters`;
+    massEl.textContent = `${inputEl} kilos = ${(inputEl * 2.204).toFixed(3)} pounds | ${inputEl} pounds = ${(inputEl * 0.454).toFixed(3)} kilos`;
 })
 
