400c69489985f1ce8fb492dec647a622a476a684
I-Slot <ismailadesholaharun@gmail.com>
2023-02-05 10:55:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,25 @@ let massEl = document.getElementById("kilogram-convert")
 
 
 convertBTN.addEventListener("click", function () {
-    console.log("clicked")
     inputEl = inputEl.value
+    // lengthEl.textContent = `${inputEl} meters = ${(inputEl * 3.281).toFixed(3)} feets | ${inputEl} feets = ${(inputEl * 0.305).toFixed(3)} meters`;
+    // volumeEl.textContent = `${inputEl} liters = ${(inputEl * 0.264).toFixed(3)} gallons | ${inputEl} gallons = ${(inputEl * 3.785).toFixed(3)} liters`;
+//     massEl.textContent = `${inputEl} kilos = ${(inputEl * 2.204).toFixed(3)} pounds | ${inputEl} pounds = ${(inputEl * 0.454).toFixed(3)} kilos`;
+    lengthElFunc()
+    volumeElFunc()
+    massElFunc()
+ })
+
+function lengthElFunc() {
     lengthEl.textContent = `${inputEl} meters = ${(inputEl * 3.281).toFixed(3)} feets | ${inputEl} feets = ${(inputEl * 0.305).toFixed(3)} meters`;
+}
+
+function volumeElFunc() {
     volumeEl.textContent = `${inputEl} liters = ${(inputEl * 0.264).toFixed(3)} gallons | ${inputEl} gallons = ${(inputEl * 3.785).toFixed(3)} liters`;
+    
+}
+
+function massElFunc() {
     massEl.textContent = `${inputEl} kilos = ${(inputEl * 2.204).toFixed(3)} pounds | ${inputEl} pounds = ${(inputEl * 0.454).toFixed(3)} kilos`;
-})
+}
 
